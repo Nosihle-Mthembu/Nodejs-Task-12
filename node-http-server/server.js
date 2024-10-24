5cc077c1bb0904bc733630c858bd05c5937d0f3d
Nosihle Mthembu <bongekam16@gmail.com>
2024-10-24 09:48:22 +0200

    Feat:Added a branch and pulled the made changes to the main

--- node-http-server/server.js
+++ node-http-server/server.js
@@ -66,8 +66,4 @@ const server = http.createServer((req, res) => {
     }
 });
 
-// Start the server on port 3000
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+
